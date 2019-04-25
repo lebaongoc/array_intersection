372d307e3a0a22dab93bf118a13ba15f5daa6ca8
Ngoc Le <ngocle@outlook.com>
2019-04-24 23:00:46 -0700

    built intersection method

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,19 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), n is the number element in the second array that we loop through to identify if it is included in the set
+# Space complexity: O(n), n is the size of the intersection array
+require "set"
+
 def intersection(array1, array2)
-  raise NotImplementedError
+  if array1 == nil || array2 == nil
+    return []
+  end
+  array1_set = Set.new(array1)
+  intersection_array = []
+
+  array2.each do |num|
+    if array1_set.include? num
+      intersection_array << num
+    end
+  end
+  return intersection_array
 end
